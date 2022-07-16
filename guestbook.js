35bb2fac1f5b6d674689252ab3b58cd82a22ef9b
moory12 <44293438+moory12@users.noreply.github.com>
2022-07-16 13:50:38 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://02830b69.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
